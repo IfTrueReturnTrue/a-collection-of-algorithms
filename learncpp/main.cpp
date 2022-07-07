df22976ec1d7f8183a5ecb2727c0afdcd0c44ebe
IfTrueReturnTrue <alanduan1234@gmail.com>
2022-07-07 20:24:41 +1000

    Allowed user to enter precision, max and min bounds.

--- learncpp/main.cpp
+++ learncpp/main.cpp
@@ -11,8 +11,17 @@ int main()
     
     //input();
     //experimental sus function
-   
-    int num = solve_sus_equation(0.00001, -1000, 500);
+    double min;
+    double max;
+    double precision;
+    cout << "Enter the minimum bound:\n";
+    cin >> min;
+    cout << "Enter the maximum bound:\n";
+    cin >> max;
+    cout << "Enter the precision (Number of decimal places):\n";
+    cin >> precision;
+    
+    int num = solve_sus_equation(pow(10, -precision), min, max);
     cout << "\n" << num << " is the number of total solutions.";
     printf("\n\nPress the ENTER key to exit...");
     getchar();
